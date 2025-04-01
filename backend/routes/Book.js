ba1e72046608fa957df868567e7666cafa89c36a
chafaa life <chafaa2024@outlook.com>
2025-04-02 01:11:30 +0200

    middlewares: auth.js & multer-config added and incorporated in books route

--- backend/routes/Book.js
+++ backend/routes/Book.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require('../controllers/middleware/auth');
+const multer = require('../controllers/middleware/multer-config')
 const Book = require("../models/Book");
 const bookCtrl = require("../controllers/Book");
-router.get("/", bookCtrl.getAllBooks);
-router.get("/:id", bookCtrl.getOneBook);
-router.post("/:id", bookCtrl.createBook);
-router.delete("/:id", bookCtrl.deleteBook);
-router.put("/:id", bookCtrl.updateBook);
+
+router.get("/",auth, bookCtrl.getAllBooks);
+router.get("/:id",auth, bookCtrl.getOneBook);
+router.post("/:id",auth,multer, bookCtrl.createBook);
+router.delete("/:id",auth, bookCtrl.deleteBook);
+router.put("/:id",auth, bookCtrl.updateBook);
 
 module.exports = router;
